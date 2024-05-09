a6c84d49d3a8dbbad59517c318bf41ab2fa47125
Sahithi-03 <114218631+Sahithi-03@users.noreply.github.com>
2024-05-09 16:32:48 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "root";
+$database = "bluebirdhotel";
+
+$conn = mysqli_connect($server,$username,$password,$database);
+
+if(!$conn){
+    die("<script>alert('connection Failed.')</script>");
+}
+
+ 
